da3c6f659b0c836c9452eb475706a32a2f60d71a
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 11:38:47 +0530

    [Aryan Jain] Profit And Loss

---
+++ core-java-practice/grc-codebase/programming-elements/profitAndLoss.java
@@ -0,0 +1,14 @@
+public class profitAndLoss {
+    public static void main(String[] args) {
+
+        double costPrice = 129.0;
+        double sellingPrice = 191.0;
+
+        System.out.println("Cost Price is: " + costPrice);
+        System.out.println("Selling Price is: " + sellingPrice);
+        System.out.println("Profit is: " + (sellingPrice - costPrice));
+        System.out.println("profit %  is: " + profit/costPrice * 100);
+     
+
+    }
+}
