20f8a80ff44d9458e9f8283fe978441bc560379f
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 12:39:53 +0530

    [Aryan Jain] Student Fees

---
+++ core-java-practice/grc-codebase/programming-elements/studentFees.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class studentFees {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input: semester fees and discount percent
+        int semesterFees = sc.nextInt();
+
+        
+        int discountPercent = sc.nextInt();
+
+        double discountAmount = (discountPercent / 100.0) * semesterFees;
+        double totalFeesAfterDiscount = semesterFees - discountAmount;
+
+        System.out.println(
+            "Discount Amount (INR): " + discountAmount +
+            "  Final Fees After Discount (INR): " + totalFeesAfterDiscount
+        );
+
+       
+    }
+}
