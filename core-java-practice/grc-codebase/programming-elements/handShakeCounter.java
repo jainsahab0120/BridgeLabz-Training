87dc958c599dd7fa793b2cf19ab05f19b98a5594
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 12:55:15 +0530

    [Aryan Jain]  Hand Shake Counter

---
+++ core-java-practice/grc-codebase/programming-elements/handShakeCounter.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class handshakeCounter {
+    public static void main(String[] args) {
+
+        
+        Scanner sc = new Scanner(System.in);
+
+        // Number of students input lena
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = sc.nextInt();
+
+        // Formula use karke maximum handshakes calculate kar rahe hain
+        int totalHandshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        // Result 
+        System.out.println(
+            "The maximum number of handshakes among "
+            + numberOfStudents + " students is "
+            + totalHandshakes
+        );
+
+        
+    }
+}
