1674d406c5e9e42a243f290dfcb93d0815e13970
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 11:56:04 +0530

    [Aryan Jain] Volume of Earth

---
+++ core-java-practice/grc-codebase/programming-elements/volumeOfEarth.java
@@ -0,0 +1,12 @@
+public class volumeOfEarth {
+    public static void main(String[] args) {
+
+        double radius = 6378.0; // in km 
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.println("The volume of the Earth in cubic Km is : " + volume );
+        System.out.println("The volume of the Earth in cubic miles is : " + volume * 1.6*1.6*1.6);
+
+
+    }
+}
