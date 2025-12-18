cfd001bb296ce7841490e0dd1535ca63d2f76de4
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 12:50:55 +0530

    [Aryan Jain] Sqaure Side Finder

---
+++ core-java-practice/grc-codebase/programming-elements/squareSideFinder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class squareSideFinder {
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+
+        // Square ka perimeter input lena
+        System.out.print("Enter perimeter of the square: ");
+        double perimeter = sc.nextDouble();
+
+        // Perimeter ko 4 se divide karke side nikal rahe hain
+        double side = perimeter / 4;
+
+        // Final output print karna
+        System.out.println(
+            "The length of the side is " + side +
+            " whose perimeter is " + perimeter
+        );
+
+        
+    }
+}
