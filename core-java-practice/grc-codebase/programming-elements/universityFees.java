c62558d951f3b65dabe9b7c340ddf27177e975ba
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 11:49:38 +0530

    [Aryan Jain] University Fees

---
+++ core-java-practice/grc-codebase/programming-elements/universityFees.java
@@ -0,0 +1,15 @@
+public class UniversityFees {
+    public static void main(String[] args) {
+
+        int semesterFees = 125000;
+        int discountPercent = 10;
+
+        double discountAmount = (discountPercent / 100.0) * semesterFees;
+        double totalFeesAfterDiscount = semesterFees - discountAmount;
+
+        System.out.println("the Discount amount in INR is " + discountAmount + "and Final Discount fees in INR " + totalFeesAfterDiscount);
+
+       
+
+    }
+}
