a9ca522652e505573b9b9f0d60fdda20cb38a6f4
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 10:57:37 +0530

    [Aryan Jain] Age Of Harry

---
+++ core-java-practice/grc-codebase/programming-elements/AgeOfHarry.java
@@ -0,0 +1,11 @@
+public class AgeOfHarry {
+    public static void main(String[] args) {
+
+        int harryPaidaHua = 2000;
+        int currentYear = 2024;
+        System.out.println("Harry ki Age: " + (currentYear - harryPaidaHua));
+
+
+
+    }
+}
