922184c1cd16ce56fcdfa33adec6e71adfb66c1f
JainSahab <aryan.ajtdl156@gmail.com>
2025-12-18 11:12:02 +0530

    [Aryan Jain] Km Convert into miles

---
+++ core-java-practice/grc-codebase/programming-elements/kmConvertor.java
@@ -0,0 +1,10 @@
+public class kmConvertor {
+    public static void main(String[] args) {
+
+        double kilometers = 10.8 ;
+        double miles = kilometers * 1.6 ;
+
+        System.out.println(" kilometers in miles is equal to " + miles);
+
+    }
+}
