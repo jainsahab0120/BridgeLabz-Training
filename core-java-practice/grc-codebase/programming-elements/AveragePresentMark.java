915480d4413234ae4117d76bcd33ab12c5b8899a
Aryan Jain <aryan.ajtdl156@gmail.com>
2025-12-19 11:03:53 +0530

    Refactor AveragePresentMark.java

---
+++ core-java-practice/grc-codebase/programming-elements/AveragePresentMark.java
@@ -0,0 +1,15 @@
+public class AveragePresentMark {
+    public static void main(String[] args) {
+
+        int marksMath = 94;
+        int marksPhysics = 95;
+        int marksChem = 96;
+        int mark4 = 92;
+        int mark5 = 88;
+
+        int totalMarks = marksMath + marksPhysics + marksChem;
+        double averageMarks = totalMarks / 3.0;
+
+        System.out.println("Average Marks of Sam in pcm is : " + averageMarks);
+    }
+}
