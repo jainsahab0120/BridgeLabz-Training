a7f9512de03b1b47d7dfb5fd25c8a49af29048a6
Aryan Jain <aryan.ajtdl156@gmail.com>
2025-12-19 11:13:06 +0530

    Refactor StudentFees.java
    Updated class name to match Java naming conventions.

--- core-java-practice/grc-codebase/programming-elements/StudentFees.java
+++ core-java-practice/grc-codebase/programming-elements/StudentFees.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 
 public class StudentFees {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        // Input: semester fees and discount percent
         int semesterFees = sc.nextInt();
-
-        
         int discountPercent = sc.nextInt();
 
         double discountAmount = (discountPercent / 100.0) * semesterFees;
