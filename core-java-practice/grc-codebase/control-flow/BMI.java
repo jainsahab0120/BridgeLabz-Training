926cef6c0a46c2533a981440858b58842ff807f9
Aryan Jain <aryan.ajtdl156@gmail.com>
2025-12-20 10:53:08 +0530

    [Aryan Jain] Add.Added level1, level2 and level3 complete

---
+++ core-java-practice/grc-codebase/control-flow/BMI.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class BMI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double weight = sc.nextDouble();
+        double heightCm = sc.nextDouble();
+
+        double heightInMeter = heightCm / 100;
+        double bmi = weight / (heightInMeter * heightInMeter);
+        System.out.println("BMI = " + bmi);
+
+        if (bmi <= 18.4) {
+            System.out.println("Status: Underweight");
+        }
+        else if (bmi >= 18.5 && bmi <= 24.9) {
+            System.out.println("Status: Normal");
+        }
+        else if (bmi >= 25.0 && bmi <= 39.9) {
+            System.out.println("Status: Overweight");
+        }
+        else {
+            System.out.println("Status: Obese");
+        }
+    }
+}
